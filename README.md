2c185c108a0168b7a1b85dfe6a4c563814349657
Freewind <nowindlee@gmail.com>
2024-01-29 17:52:55 +0800

    typescript-typed-validator-zod-with-existing-enum-demo

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-TypeScript Typed Validator zod Demo
+TypeScript Typed Validator zod with Existing Enum Demo
 ===========================
 
-据说在type层面有点慢，但通常使用应该没什么问题。生态比较好。
-
-api还比较简单，报错也可以，但是不能直接生成json schema，需要用插件
+zod对本地enum的支持也比较好
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
 
--- hello.ts
+++ hello.ts
@@ -1,7 +1,12 @@
 import { number, string, z } from 'zod';
 
+enum Gender {
+    Male = 1,
+    Femail = 2
+}
 const user = z.object({
     username: z.union([string(), number()]),
+    gender: z.nativeEnum(Gender).optional()
 })
 
 
@@ -10,16 +15,12 @@ console.log(user);
 type User = z.infer<typeof user>;
 
 const user1: User = {
-    username: 'Freewind'
+    username: 'Freewind',
+    gender: Gender.Male,
 }
 
-const user2: User = {
-    username: 123
-}
 
-console.log(user.parse({ username: 111 }));
-
-const error = user.safeParse({ username: true })
+const error = user.safeParse({ username: true, gender: 123 })
 console.log(JSON.stringify(error, null, 4));
 
 
